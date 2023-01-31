fe30151e288d7656f0ddbd6fda2307b58964d04d
Sami Skouri <sami.skouri@gmail.com>
2023-01-31 16:21:32 +0100

    remove pathch and option

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
   // eslint-disable-next-line no-undef
     res.setHeader("Access-Control-Allow-Origin", process.env.PIQUANTEWEBSITE);
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     next();
   });
 
